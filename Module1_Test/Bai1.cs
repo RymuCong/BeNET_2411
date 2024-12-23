ababeac8c9babcce2e4a6e283192c0dc0e92c41c
cong-dep-trai <phamthanhcong0709@gmail.com>
2024-12-23 14:38:26 +0700

    module 1 test

---
+++ Module1_Test/Bai1.cs
@@ -0,0 +1,39 @@
+﻿namespace Module1_Test;
+
+using System;
+using System.Collections.Generic;
+
+public class Bai1
+{
+    public static void Main()
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        Console.WriteLine("Nhập số lượng phần tử của mảng:");
+        var n = int.Parse(Console.ReadLine());
+        int[] array = new int[n];
+        Console.WriteLine("Nhập các phần tử của mảng:");
+        for (var i = 0; i < n; i++)
+        {
+            array[i] = int.Parse(Console.ReadLine());
+        }
+        
+        Dictionary<int, int> dem = new Dictionary<int, int>();
+        foreach (var e in array)
+        {
+            if (dem.ContainsKey(e))
+            {
+                dem[e]++;
+            }
+            else
+            {
+                dem.Add(e, 1);
+            }
+        }
+        
+        Console.WriteLine("Số lần xuất hiện của mỗi phần tử trong mảng:");
+        foreach (var pair in dem)
+        {
+            Console.WriteLine($"Phần tử {pair.Key} xuất hiện {pair.Value} lần.");
+        }
+    }
+}
